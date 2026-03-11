8805e7653eb10d8a1158d8f9d6c5daa300ca4d04
borodamd <borodamd@users.noreply.github.com>
2026-03-11 08:58:58 +0200

    Create capacitive_water_sensor.h

---
+++ components/capacitive_water_sensor/capacitive_water_sensor.h
@@ -0,0 +1,54 @@
+#pragma once
+
+#include "esphome/core/component.h"
+#include "esphome/core/hal.h"
+#include "esphome/sensor/sensor.h"
+#include <CapacitiveSensor.h>
+
+namespace esphome {
+namespace capacitive_water_sensor {
+
+/**
+ * Компонент емкостного датчика воды
+ * Эмулирует работу оригинального датчика увлажнителя Xiaomi Smartmi
+ */
+class CapacitiveWaterSensor : public PollingComponent, public sensor::Sensor {
+ public:
+  /**
+   * Конструктор
+   * @param send_pin - пин для отправки сигнала
+   * @param receive_pin - пин для приема сигнала (через резистор 1МОм)
+   * @param update_interval_ms - интервал обновления в миллисекундах
+   */
+  CapacitiveWaterSensor(uint8_t send_pin, uint8_t receive_pin, uint32_t update_interval_ms);
+
+  // Методы жизненного цикла компонента
+  void setup() override;
+  void update() override;
+  void dump_config() override;
+
+  // Сеттеры для конфигурационных параметров
+  void set_num_samples(uint32_t samples) { samples_ = samples; }
+  void set_min_raw_value(uint32_t min_raw) { min_raw_ = min_raw; }
+  void set_max_raw_value(uint32_t max_raw) { max_raw_ = max_raw; }
+  void set_shorted_value(uint8_t value) { shorted_value_ = value; }
+  void set_timeout_ms(uint32_t timeout) { timeout_ms_ = timeout; }
+
+ protected:
+  // Пины подключения
+  uint8_t send_pin_;
+  uint8_t receive_pin_;
+  
+  // Параметры измерения
+  uint32_t samples_{1000};
+  uint32_t min_raw_{4200};
+  uint32_t max_raw_{11000};
+  uint8_t shorted_value_{125};
+  uint32_t timeout_ms_{500};
+  
+  // Указатель на объект библиотеки CapacitiveSensor
+  CapacitiveSensor *sensor_{nullptr};
+};
+
+}  // namespace capacitive_water_sensor
+}  // namespace esphome
