cb8753894f28faa6aad0d05e1cd3c4f7271b6bb3
borodamd <borodamd@users.noreply.github.com>
2026-03-11 09:12:15 +0200

    Update sensor.py

--- components/capacitive_water_sensor/sensor.py
+++ components/capacitive_water_sensor/sensor.py
@@ -1,52 +1,73 @@
-// components/capacitive_water_sensor/capacitive_sensor_impl.h
-#pragma once
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.components import sensor
+from esphome.const import (
+    CONF_ID,
+    CONF_UPDATE_INTERVAL,
+    DEVICE_CLASS_EMPTY,
+    STATE_CLASS_MEASUREMENT,
+    UNIT_EMPTY
+)
+from esphome.core import CORE
 
-#include <Arduino.h>
+# Определение конфигурационных констант
+CONF_SEND_PIN = "send_pin"
+CONF_RECEIVE_PIN = "receive_pin"
+CONF_NUM_SAMPLES = "num_samples"
+CONF_MIN_RAW = "min_raw_value"
+CONF_MAX_RAW = "max_raw_value"
+CONF_TIMEOUT = "timeout_ms"
+CONF_SHORTED_VALUE = "shorted_value"
 
-class SimpleCapacitiveSensor {
-private:
-    int sendPin;
-    int receivePin;
-    unsigned long timeout;
+# Создание пространства имен для C++ кода
+capacitive_water_sensor_ns = cg.esphome_ns.namespace("capacitive_water_sensor")
+CapacitiveWaterSensor = capacitive_water_sensor_ns.class_(
+    "CapacitiveWaterSensor", sensor.Sensor, cg.PollingComponent
+)
 
-public:
-    SimpleCapacitiveSensor(int send, int receive) {
-        sendPin = send;
-        receivePin = receive;
-        timeout = 500000; // 500 ms в микросекундах
-    }
+# Схема конфигурации
+CONFIG_SCHEMA = sensor.sensor_schema(
+    CapacitiveWaterSensor,
+    accuracy_decimals=1,
+    device_class=DEVICE_CLASS_EMPTY,
+    state_class=STATE_CLASS_MEASUREMENT,
+    unit_of_measurement=UNIT_EMPTY
+).extend({
+    cv.GenerateID(): cv.declare_id(CapacitiveWaterSensor),
+    cv.Required(CONF_SEND_PIN): cv.int_,
+    cv.Required(CONF_RECEIVE_PIN): cv.int_,
+    cv.Optional(CONF_NUM_SAMPLES, default=100): cv.int_range(min=1, max=1000),
+    cv.Optional(CONF_MIN_RAW, default=4200): cv.int_,
+    cv.Optional(CONF_MAX_RAW, default=11000): cv.int_,
+    cv.Optional(CONF_SHORTED_VALUE, default=125): cv.int_range(min=100, max=255),
+    cv.Optional(CONF_TIMEOUT, default=500): cv.int_range(min=1, max=5000),
+}).extend(cv.polling_component_schema("500ms"))
+
+async def to_code(config):
+    """Генерация C++ кода из YAML конфигурации"""
     
-    void set_CS_Timeout_Millis(unsigned long ms) {
-        timeout = ms * 1000L;
-    }
+    # Создаем переменную компонента
+    var = cg.new_Pvariable(
+        config[CONF_ID],
+        config[CONF_SEND_PIN],
+        config[CONF_RECEIVE_PIN],
+        config[CONF_UPDATE_INTERVAL].total_milliseconds,
+    )
     
-    void set_CS_AutocaL_Millis(unsigned long) {
-        // Отключаем автокалибровку
-    }
+    # Регистрируем компонент
+    await cg.register_component(var, config)
+    await sensor.register_sensor(var, config)
     
-    long capacitiveSensorRaw(unsigned int samples) {
-        long total = 0;
-        pinMode(receivePin, INPUT);
-        pinMode(sendPin, OUTPUT);
-        
-        for (unsigned int i = 0; i < samples; i++) {
-            digitalWrite(sendPin, HIGH);
-            unsigned long start = micros();
-            unsigned long end = start;
-            
-            while (digitalRead(receivePin) == LOW && (end - start) < timeout) {
-                end = micros();
-            }
-            
-            total += (end - start);
-            
-            digitalWrite(sendPin, LOW);
-            delayMicroseconds(10);
-        }
-        
-        pinMode(sendPin, INPUT);
-        
-        if (total == 0) return -2; // Короткое замыкание
-        return total / samples;
-    }
-};
+    # Устанавливаем параметры
+    cg.add(var.set_num_samples(config[CONF_NUM_SAMPLES]))
+    cg.add(var.set_min_raw_value(config[CONF_MIN_RAW]))
+    cg.add(var.set_max_raw_value(config[CONF_MAX_RAW]))
+    cg.add(var.set_shorted_value(config[CONF_SHORTED_VALUE]))
+    cg.add(var.set_timeout_ms(config[CONF_TIMEOUT]))
+    
+    # Добавляем библиотеку CapacitiveSensor
+    cg.add_library("PaulStoffregen/CapacitiveSensor", "0.5.1")
+    
+    # Добавляем необходимые define для ESP32
+    if CORE.is_esp32:
+        cg.add_define("ESP32")
