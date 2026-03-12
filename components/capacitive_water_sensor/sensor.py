d9e11d443876b6ade106e20ade7f4ae78c41b4d5
borodamd <borodamd@users.noreply.github.com>
2026-03-12 07:44:52 +0200

    Update sensor.py

--- components/capacitive_water_sensor/sensor.py
+++ components/capacitive_water_sensor/sensor.py
@@ -9,22 +9,22 @@ from esphome.const import (
     UNIT_EMPTY
 )
 
-# Определение конфигурационных констант
+# Константы
 CONF_SEND_PIN = "send_pin"
 CONF_RECEIVE_PIN = "receive_pin"
 CONF_NUM_SAMPLES = "num_samples"
-CONF_MIN_RAW = "min_raw_value"
-CONF_MAX_RAW = "max_raw_value"
 CONF_TIMEOUT = "timeout_ms"
+CONF_USE_SOFT_THRESHOLD = "use_soft_threshold"
+CONF_DRY_THRESHOLD = "dry_threshold"
+CONF_WET_MIN_RAW = "wet_min_raw"
+CONF_WET_MAX_RAW = "wet_max_raw"
 CONF_SHORTED_VALUE = "shorted_value"
 
-# Создание пространства имен для C++ кода
 capacitive_water_sensor_ns = cg.esphome_ns.namespace("capacitive_water_sensor")
 CapacitiveWaterSensor = capacitive_water_sensor_ns.class_(
     "CapacitiveWaterSensor", sensor.Sensor, cg.PollingComponent
 )
 
-# Схема конфигурации
 CONFIG_SCHEMA = sensor.sensor_schema(
     CapacitiveWaterSensor,
     accuracy_decimals=1,
@@ -35,17 +35,16 @@ CONFIG_SCHEMA = sensor.sensor_schema(
     cv.GenerateID(): cv.declare_id(CapacitiveWaterSensor),
     cv.Required(CONF_SEND_PIN): cv.int_,
     cv.Required(CONF_RECEIVE_PIN): cv.int_,
-    cv.Optional(CONF_NUM_SAMPLES, default=100): cv.int_range(min=1, max=1000),
-    cv.Optional(CONF_MIN_RAW, default=4200): cv.int_,
-    cv.Optional(CONF_MAX_RAW, default=11000): cv.int_,
+    cv.Optional(CONF_NUM_SAMPLES, default=10): cv.int_range(min=5, max=30),
+    cv.Optional(CONF_TIMEOUT, default=100): cv.int_range(min=50, max=300),
+    cv.Optional(CONF_USE_SOFT_THRESHOLD, default=True): cv.boolean,
+    cv.Optional(CONF_DRY_THRESHOLD, default=300): cv.int_range(min=100, max=1000),
+    cv.Optional(CONF_WET_MIN_RAW, default=0): cv.int_,
+    cv.Optional(CONF_WET_MAX_RAW, default=300): cv.int_,
     cv.Optional(CONF_SHORTED_VALUE, default=125): cv.int_range(min=100, max=255),
-    cv.Optional(CONF_TIMEOUT, default=500): cv.int_range(min=1, max=5000),
-}).extend(cv.polling_component_schema("500ms"))
+}).extend(cv.polling_component_schema("3s"))
 
 async def to_code(config):
-    """Генерация C++ кода из YAML конфигурации"""
-    
-    # Создаем переменную компонента
     var = cg.new_Pvariable(
         config[CONF_ID],
         config[CONF_SEND_PIN],
@@ -53,15 +52,13 @@ async def to_code(config):
         config[CONF_UPDATE_INTERVAL].total_milliseconds,
     )
     
-    # Регистрируем компонент
     await cg.register_component(var, config)
     await sensor.register_sensor(var, config)
     
-    # Устанавливаем параметры
     cg.add(var.set_num_samples(config[CONF_NUM_SAMPLES]))
-    cg.add(var.set_min_raw_value(config[CONF_MIN_RAW]))
-    cg.add(var.set_max_raw_value(config[CONF_MAX_RAW]))
-    cg.add(var.set_shorted_value(config[CONF_SHORTED_VALUE]))
     cg.add(var.set_timeout_ms(config[CONF_TIMEOUT]))
-    
-    # Убираем cg.add_library - теперь используем свою реализацию
+    cg.add(var.set_use_soft_threshold(config[CONF_USE_SOFT_THRESHOLD]))
+    cg.add(var.set_dry_threshold(config[CONF_DRY_THRESHOLD]))
+    cg.add(var.set_wet_min_raw(config[CONF_WET_MIN_RAW]))
+    cg.add(var.set_wet_max_raw(config[CONF_WET_MAX_RAW]))
+    cg.add(var.set_shorted_value(config[CONF_SHORTED_VALUE]))
