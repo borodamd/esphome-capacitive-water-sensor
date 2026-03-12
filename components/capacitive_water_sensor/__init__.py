d41bbd3f0826ef576951af993e3b0e49a571dee0
borodamd <borodamd@users.noreply.github.com>
2026-03-12 11:11:16 +0200

    Update __init__.py

--- components/capacitive_water_sensor/__init__.py
+++ components/capacitive_water_sensor/__init__.py
@@ -1,1 +1,9 @@
-"""Capacitive Water Sensor component for ESPHome."""
+import esphome.codegen as cg
+import esphome.config_validation as cv
+
+CODEOWNERS = ["@your_github_username"]
+capacitive_water_sensor_ns = cg.esphome_ns.namespace("capacitive_water_sensor")
+
+CONFIG_SCHEMA = cv.All(
+    cv.Schema({}),
+)
