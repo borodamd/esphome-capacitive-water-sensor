54852c71669aba4f5aa126d80c7365f3b082e72f
borodamd <borodamd@users.noreply.github.com>
2026-03-12 11:11:32 +0200

    Update sensor.py

--- components/capacitive_water_sensor/sensor.py
+++ components/capacitive_water_sensor/sensor.py
@@ -1,54 +1,30 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import sensor
-from esphome.const import (
-    CONF_ID,
-    CONF_UPDATE_INTERVAL,
-    DEVICE_CLASS_EMPTY,
-    STATE_CLASS_MEASUREMENT,
-    UNIT_EMPTY
-)
+from esphome.const import CONF_ID, STATE_CLASS_MEASUREMENT, UNIT_EMPTY
 
-CONF_SEND_PIN = "send_pin"
-CONF_RECEIVE_PIN = "receive_pin"
-CONF_NUM_SAMPLES = "num_samples"
-CONF_TIMEOUT_MS = "timeout_ms"
-CONF_SHORTED_VALUE = "shorted_value"
+DEPENDENCIES = []
 
 capacitive_water_sensor_ns = cg.esphome_ns.namespace("capacitive_water_sensor")
 CapacitiveWaterSensor = capacitive_water_sensor_ns.class_(
-    "CapacitiveWaterSensor", sensor.Sensor, cg.PollingComponent
+    "CapacitiveWaterSensor", cg.PollingComponent, sensor.Sensor
 )
 
 CONFIG_SCHEMA = sensor.sensor_schema(
     CapacitiveWaterSensor,
-    accuracy_decimals=1,
-    device_class=DEVICE_CLASS_EMPTY,
+    unit_of_measurement=UNIT_EMPTY,
+    accuracy_decimals=0,
     state_class=STATE_CLASS_MEASUREMENT,
-    unit_of_measurement=UNIT_EMPTY
 ).extend({
     cv.GenerateID(): cv.declare_id(CapacitiveWaterSensor),
-    cv.Required(CONF_SEND_PIN): cv.int_,
-    cv.Required(CONF_RECEIVE_PIN): cv.int_,
-    cv.Optional(CONF_NUM_SAMPLES, default=200): cv.int_range(min=10, max=500),
-    cv.Optional(CONF_TIMEOUT_MS, default=500): cv.int_range(min=50, max=2000),
-    cv.Optional(CONF_SHORTED_VALUE, default=125): cv.int_range(min=100, max=255),
-}).extend(cv.polling_component_schema("2s"))
+    cv.Required("sender_pin"): cv.int_,
+    cv.Required("sensor_pin"): cv.int_,
+}).extend(cv.polling_component_schema("100ms"))
 
-async def to_code(config):
-    var = cg.new_Pvariable(
-        config[CONF_ID],
-        config[CONF_SEND_PIN],
-        config[CONF_RECEIVE_PIN],
-        config[CONF_UPDATE_INTERVAL].total_milliseconds,
-    )
-    
-    await cg.register_component(var, config)
-    await sensor.register_sensor(var, config)
-    
-    cg.add(var.set_num_samples(config[CONF_NUM_SAMPLES]))
-    cg.add(var.set_timeout_ms(config[CONF_TIMEOUT_MS]))
-    cg.add(var.set_shorted_value(config[CONF_SHORTED_VALUE]))
-    
-    # Добавляем библиотеку
-    cg.add_library("PaulStoffregen/CapacitiveSensor", "0.5.1")
+def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID])
+    yield cg.register_component(var, config)
+    yield sensor.register_sensor(var, config)
+
+    cg.add(var.set_pins(config["sender_pin"], config["sensor_pin"]))
+    cg.add_library("CapacitiveSensor", None)
