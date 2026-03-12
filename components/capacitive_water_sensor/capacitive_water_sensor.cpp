2fc038b0d84c450abb33823a2f5a4824c4a773ee
borodamd <borodamd@users.noreply.github.com>
2026-03-12 11:15:36 +0200

    Update capacitive_water_sensor.cpp

--- components/capacitive_water_sensor/capacitive_water_sensor.cpp
+++ components/capacitive_water_sensor/capacitive_water_sensor.cpp
@@ -10,18 +10,21 @@ void CapacitiveWaterSensor::setup() {
   sensor_impl_ = std::make_unique<CapacitiveSensor>(sender_pin_, sensor_pin_);
   sensor_impl_->set_CS_AutocaL_Millis(0xFFFFFFFF);
   sensor_impl_->set_CS_Timeout_Millis(500);
+  
+  // В ESP8266 Serial.begin() может конфликтовать с логгером.
+  // Мы полагаемся на то, что в YAML baud_rate: 0, тогда Serial свободен.
   Serial.begin(9600);
 }
 
 void CapacitiveWaterSensor::update() {
-  long reading_raw = sensor_impl_->capacitiveSensorRaw(1000);
+  // Передаем samples_ явно
+  long reading_raw = sensor_impl_->capacitiveSensorRaw(this->samples_);
   uint8_t mapped_value;
 
   if (reading_raw == -2) {
     mapped_value = 125;
   } else {
     long val = reading_raw;
-    // Используем пока значения из вашего скетча
     if (val < 4200) val = 4200;
     if (val > 11000) val = 11000;
     mapped_value = (uint8_t)((val - 4200) * 120 / (11000 - 4200));
@@ -34,6 +37,8 @@ void CapacitiveWaterSensor::update() {
   packet_[42] = checksum;
 
   Serial.write(packet_, 43);
+  Serial.flush(); // Добавим для уверенности отправки
+  
   this->publish_state(reading_raw);
 }
 
